cc281c30661e2005be432ad9bbd06fe4b54831a7
Igor <igor_pavlenko@yahoo.com>
2019-02-01 10:32:02 +0100

    rendering a card to the ui

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Create and shuffle a new deck of cards
+
+  const hitButton = document.querySelector('.js-hit');
+
   const deck = new Deck();
   deck.shuffle();
+  
+  const playerParent = document.querySelector('.cards-player');
+  
+  let cardFromStack = deck.cards[deck.cards.length - 1]
+  
+  hitButton.addEventListener('click', () => {
+    const card = new Card(cardFromStack.rank, cardFromStack.suit);
+    card.mount(playerParent)
+    console.log(card);
+  })
+	
+
+
   console.log(deck);
 });
--- src/assets/js/card.js
+++ src/assets/js/card.js
@@ -6,4 +6,24 @@ class Card {
     this.rank = rank;
     this.suit = suit;
   }
+
+  
+  render () {
+    this.element = document.createElement('div');
+    this.element.className = `card face-${this.rank}-of-${this.suit}`;
+
+    return this.element;
+  }
+
+  update() {
+
+  }
+
+  mount(parent) {
+    parent.appendChild(this.render());
+  }
+
+
+
+
 } 
